b2b4d7587d92de97ea93f7823a0b2dc473daea2a
Tom Dryer <tomdryer.com@gmail.com>
2012-11-09 20:52:50 -0800

    Stop xmobar from freezing everything

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,31 +1,23 @@
--- Imports.
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.IM
 import XMonad.Layout.Grid
 import XMonad.Layout.Reflect
+import XMonad.Hooks.ManageDocks
 
--- The main function.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+main = do
+    xmonad myConfig
 
--- Command to launch the bar.
-myBar = "xmobar"
-
--- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
-
--- Key binding to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
-
--- Main configuration, override the defaults to your liking.
 myConfig = defaultConfig
-    { modMask = mod4Mask
+    { modMask = mod4Mask -- use super key
     , layoutHook = myLayout
     , borderWidth = 2
+    , startupHook = spawn "xmobar"
     }
 
-myLayout = tiled ||| Mirror tiled ||| Full ||| withIM (1/10) (Title "Buddy List") doubleTiled
+myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full ||| im_double)
     where
+        im_double = withIM (1/10) (Title "Buddy List") doubleTiled
         doubleTiled = Tall 2 delta ratio
         -- default tiling algorithm partitions the screen into two panes
         tiled   = Tall nmaster delta ratio
