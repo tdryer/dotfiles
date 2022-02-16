5d26dceabfb483d0aa9e0c5d233c2753a72f0380
Tom Dryer <tomdryer.com@gmail.com>
2022-02-15 22:04:12 -0800

    kitty: Match cursor position in scrollback pager

---
+++ config/kitty/scrollback-pager.bash
@@ -0,0 +1,8 @@
+# wrapper script to use vim as kitty's scrollback pager while matching the
+# current cursor position from the terminal
+
+vim \
+    -c "norm G" \
+    -c "call cursor($(expr $1 + $2), $3)" \
+    -c "set nonumber nolist buftype=nofile" \
+    -
