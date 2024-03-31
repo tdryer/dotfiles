97a2118c6b2fedda359c32d425ab55cc90b61564
Tom Dryer <tomdryer.com@gmail.com>
2024-03-30 22:17:25 -0700

    nvim: Fix wrapping Python comments with `gq`

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -5,5 +5,9 @@ vim.lsp.start({
     settings = { pylsp = { plugins = { ruff = {
         enabled = true,
         extendSelect = { 'ARG' },
-    } } } }
+    } } } },
+    on_attach = function(client, bufnr)
+        -- Do not use lsp for `gq` formatting
+        vim.api.nvim_buf_set_option(bufnr, "formatexpr", "")
+    end,
 })
