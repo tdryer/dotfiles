8ed9b62e8cd94964764883a88f47817f0e85d5a2
Tom Dryer <tomdryer.com@gmail.com>
2024-09-06 22:05:38 -0700

    nvim: Fix auto-wrapping markdown bullets

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,6 @@
--- insert bullets when starting new line
+-- insert bullets when starting new line via comment prefixes
 -- use C-U to cancel, C-T to indent, C-D to outdent
+-- TODO: Make auto-wrapping with `gq` indent correctly
 vim.opt_local.comments = {
     "b:* [ ]",
     "b:* [X]",
@@ -11,6 +12,8 @@ vim.opt_local.comments = {
     "b:-",
 }
 vim.opt_local.formatoptions:append("ro")
+-- don't auto-wrap text with comment prefixes
+vim.opt_local.formatoptions:remove("q")
 vim.api.nvim_set_hl(0, "markdownCode", { link="String" })
 vim.api.nvim_set_hl(0, "markdownCodeBlock", { link="String" })
 vim.treesitter.start()
