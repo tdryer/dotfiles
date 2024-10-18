1eb55f5686ab0af7d667f47a73b4464515a420a8
Tom Dryer <tomdryer.com@gmail.com>
2024-10-17 22:20:42 -0700

    nvim: Fix multi-file editing with gopls

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -1,4 +1,10 @@
 -- install: go install golang.org/x/tools/gopls@latest
-vim.lsp.start({ name = 'gopls', cmd = { 'gopls' } })
+vim.lsp.start({
+    name = 'gopls',
+    cmd = { 'gopls' },
+    root_dir = vim.fs.dirname(
+        vim.fs.find({ 'go.mod', 'go.work', '.git' }, { upward = true })[1]
+    ),
+})
 vim.opt_local.expandtab = false
 vim.opt_local.list = false
