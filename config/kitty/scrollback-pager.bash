90c5d1f0f520a592dae3b70469607556a1abf740
Tom Dryer <tomdryer.com@gmail.com>
2025-07-14 22:43:32 -0700

    kitty: Change scrollback pager to nvim

--- config/kitty/scrollback-pager.bash
+++ config/kitty/scrollback-pager.bash
@@ -1,7 +1,7 @@
 # wrapper script to use vim as kitty's scrollback pager while matching the
 # current cursor position from the terminal
 
-vim \
+nvim \
     -c "norm G" \
     -c "call cursor($(expr $1 + $2), $3)" \
     -c "set nonumber nolist buftype=nofile" \
